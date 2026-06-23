3847effd953c772a76380e08adf143ed10d1eca5
Abhinraj <144911680+abhinraj23@users.noreply.github.com>
2026-06-23 18:08:37 +0000

    initial ZIP pricing api

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+PRICE_MAP = {
+    "75028": 1499,
+    "10001": 1699,
+    "90210": 1799
+}
+
+@app.get("/price")
+def get_price(zip_code: str):
+    return {
+        "price": PRICE_MAP.get(zip_code, 1999)
+    }
